f8f3efc35daa2112c32a15cb25b00a5aa7eae8b1
napleon <798936274@qq.com>
2023-11-13 19:38:54 +0800

    lab3: print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void print_level(int level) {
+  switch (level) {
+    case 0: printf("..%d: \n", level);
+    case 1: printf(".. ..%d: \n", level);
+    case 2: printf(".. .. ..%d: \n", level);
+    default: break;
+  }
+}
+
+void print_process(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      uint64 child = PTE2PA(pte);
+      print_level(level);
+      printf("pte %p pa %p\n", i, pte, child);
+      print_process((pagetable_t)child,level+1);
+    } else if (pte & PTE_V) {
+      uint64 pa = PTE2PA(pte);
+      print_level(level);
+      printf("%d: pte %p pa %p\n",i,pte,pa);
+    }
+  }
+}
+
+// visualize RISC-V page tables
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  print_process(pagetable,0);
+}
